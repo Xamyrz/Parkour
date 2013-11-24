25b9006b7aa60ef5a0e74e2260b6ce46bbe920ec
maciekmm <maciekmm@maciekmmnet>
2013-11-24 11:52:52 +0100

    Accidently put wrong license text

--- src/main/java/me/cmastudios/mcparkour/commands/HighscoresCommand.java
+++ src/main/java/me/cmastudios/mcparkour/commands/HighscoresCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 maciekmm
+ * Copyright (C) 2013 Connor Monahan
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package me.cmastudios.mcparkour.commands;
 
 import java.sql.PreparedStatement;

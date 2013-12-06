7a57fe1db3614069130bf79f35477c6d944b6220
cmastudios <admin@cmastudios.me>
2013-12-05 20:15:46 -0600

    Do not remove names from copyright notices

--- src/main/java/me/cmastudios/mcparkour/commands/HighscoresCommand.java
+++ src/main/java/me/cmastudios/mcparkour/commands/HighscoresCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 maciekmm
+ * Copyright (C) 2013 Connor Monahan and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

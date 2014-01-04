33d561711dd35c8199dafc378069968dfd4d14bb
Maciej Mionskowski <m.mionskowski@maciekmm.tk>
2014-01-04 19:06:56 +0100

    The branch was messed up on bitbucket Reverting revert
    This reverts commit 6669b499503285c293a724c1361ba770abd2aa5b.

---
+++ src/main/java/me/cmastudios/mcparkour/events/PlayerCompleteParkourEventBuilder.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2013 Maciej Mionskowski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.cmastudios.mcparkour.events;
+
+import me.cmastudios.mcparkour.Parkour;
+import me.cmastudios.mcparkour.data.PlayerHighScore;
+
+public class PlayerCompleteParkourEventBuilder {
+    private Parkour.PlayerCourseData endData;
+    private PlayerHighScore highScore;
+    private long completionTime;
+    private boolean isPersonalBest;
+    private boolean isBest;
+    private double xp;
+    private boolean isTopTen;
+
+
+    public PlayerCompleteParkourEventBuilder(Parkour.PlayerCourseData endData,PlayerHighScore highScore,long completionTime) {
+        this.endData = endData;
+        this.highScore = highScore;
+        this.completionTime = completionTime;
+    }
+
+    public void setPersonalBest(boolean isPersonalBest) {
+        this.isPersonalBest = isPersonalBest;
+    }
+
+    public void setBest (boolean isBest) {
+        this.isBest = isBest;
+    }
+
+    public PlayerCompleteParkourEvent getEvent() {
+        return new PlayerCompleteParkourEvent(endData,highScore,completionTime,isPersonalBest,isBest,xp,isTopTen);
+    }
+
+    public void setReducedXp(double xp) {
+        this.xp = xp;
+    }
+
+    public void setTopTen(boolean isTopTen) {
+        this.isTopTen = isTopTen;
+    }
+}
---
+++ src/main/java/me/cmastudios/mcparkour/events/PlayerStartParkourEvent.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2013 Maciej Mionskowski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.cmastudios.mcparkour.events;
+
+import me.cmastudios.mcparkour.Parkour;
+import me.cmastudios.mcparkour.data.ParkourCourse;
+import me.cmastudios.mcparkour.data.PlayerExperience;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerStartParkourEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    private Player player;
+    private Parkour.PlayerCourseData data;
+    private PlayerExperience xp;
+
+    public PlayerStartParkourEvent(Player player,PlayerExperience xp, Parkour.PlayerCourseData data) {
+        this.player = player;
+        this.data = data;
+        this.xp = xp;
+
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public ParkourCourse getCourse() {
+        return data.course;
+    }
+
+    public PlayerExperience getXp() {
+        return xp;
+    }
+
+}
